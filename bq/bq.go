270bd5f98d0b9619238f7f09f496daf9226c265b
n.boughton <nicholasboughton@googlemail.com>
2016-10-03 14:14:57 +0100

    Massively simplified code with goquery

--- bq/bq.go
+++ bq/bq.go
@@ -2,10 +2,9 @@ package bq
 
 import (
 	"fmt"
-	"net/http"
 	"strings"
 
-	"golang.org/x/net/html"
+	"github.com/PuerkitoBio/goquery"
 )
 
 // Quote contains the extracted quote data
@@ -24,64 +23,21 @@ func Search(words []string, pages int) ([]Quote, error) {
 
 	// get the search results
 	for i := 1; i <= pages; i++ {
-		qFound := 0
-		resp, err := http.Get(fmt.Sprintf("%v?q=%v&pg=%v", bQuoteURL, q, i))
-		if err != nil {
-			return quotes, err
-		}
-		defer resp.Body.Close()
-
-		doc, err := html.Parse(resp.Body)
+		doc, err := goquery.NewDocument(fmt.Sprintf("%v?q=%v&pg=%v", bQuoteURL, q, i))
 		if err != nil {
 			return quotes, err
 		}
 
-		var (
-			getTextAndAuthor func(*html.Node)
-			findQuotes       func(*html.Node)
-			text, author     string
-		)
-
-		findQuotes = func(n *html.Node) {
-			if n.Type == html.ElementNode && n.Data == "div" {
-				for _, attr := range n.Attr {
-					if attr.Key == "class" && strings.Contains(attr.Val, "bqQt") {
-						getTextAndAuthor(n)
-						if len(text) > 0 && len(author) > 0 {
-							quotes = append(quotes, Quote{Author: author, Text: text})
-							text, author = "", ""
-							qFound++
-						}
-						break
-					}
-				}
+		doc.Find(".bqQt").Each(func(idx int, item *goquery.Selection) {
+			qt := Quote{
+				Text:   item.Find(".bqQuoteLink").Children().First().Text(),
+				Author: item.Find(".bq-aut").Children().First().Text(),
 			}
-
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				findQuotes(c)
+			if len(qt.Text) > 0 && len(qt.Author) > 0 {
+				quotes = append(quotes, qt)
 			}
-		}
+		})
 
-		getTextAndAuthor = func(n *html.Node) {
-			if n.Type == html.ElementNode && (n.Data == "span" || n.Data == "div") {
-				for _, attr := range n.Attr {
-					if attr.Key == "class" && attr.Val == "bqQuoteLink" {
-						text = n.FirstChild.FirstChild.Data
-					} else if attr.Key == "class" && attr.Val == "bq-aut" {
-						author = n.FirstChild.FirstChild.Data
-					}
-				}
-			}
-
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				getTextAndAuthor(c)
-			}
-		}
-
-		findQuotes(doc)
-		if qFound == 0 {
-			break
-		}
 	}
 
 	if len(quotes) > 0 {
